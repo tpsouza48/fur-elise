645e034b4a6a49a9d5323b71112fc4a10779dc77
Thiago Souza <tpimentel368@gmail.com>
2024-05-28 23:21:23 -0300

    Estilização inicial e protótipo

---
+++ script.js
@@ -0,0 +1,34 @@
+let slideIndex = 0;
+
+function criarFolhas(){
+    let numeroFolhas = 50;
+
+    for(let i = 0; i < numeroFolhas; i++){
+        let folha = document.createElement("i");
+        document.getElementById("leaves").append(folha);
+    }
+}
+
+function mostrarSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slides");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    
+    slides[slideIndex-1].style.display = "block";
+    
+    setTimeout(mostrarSlides, 3000);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    criarFolhas();
+    mostrarSlides();
+})
